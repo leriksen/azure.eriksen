87f696bcc0f60bb211a17db57bc858af3011b061
Leif Eriksen <leif.eriksen.au@gmail.com>
2020-02-14 15:39:50 +1100

    move to symlink organisation

--- project/main.tf
+++ project/main.tf
@@ -10,49 +10,49 @@ terraform {
 }
 
 module "global" {
-  source = "../const/global"
+  source = "./global"
 }
 
 module "environment" {
-  source = "../const/environment"
-  environment = "${var.TERRAFORM_WORKSPACE}"
+  source = "./environment"
+  environment = var.TERRAFORM_WORKSPACE
 }
 
 provider "azurerm" {
-  tenant_id       = "${module.global.tenant_id}"
-  subscription_id = "${module.environment.subscription}"
+  tenant_id       = module.global.tenant_id
+  subscription_id = module.environment.subscription
   version         = "~> 1.0"
 }
 
 resource "azurerm_resource_group" "workspace-rg" {
-  location = "${module.environment.location}"
-  name     = "${module.global.prefix}-${var.TERRAFORM_WORKSPACE}"
-  tags     = "${module.global.tags}"
+  location = "module.environment.location
+  name     = "${module.global.prefix}-${var.TERRAFORM_WORKSPACE}
+  tags     = module.global.tags
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-${var.TERRAFORM_WORKSPACE}"
-  resource_group_name = "${azurerm_resource_group.workspace-rg.name}"
+  resource_group_name = azurerm_resource_group.workspace-rg.name
   address_space       = [
     "10.0.0.0/16"
   ]
-  location            = "${module.environment.location}"
-  tags                = "${module.global.tags}"
+  location            = module.environment.location
+  tags                = module.global.tags
 }
 
 resource "azurerm_network_security_group" "nsg" {
   name                = "nsg-${var.TERRAFORM_WORKSPACE}"
-  location            = "${module.environment.location}"
-  resource_group_name = "${azurerm_resource_group.workspace-rg.name}"
+  location            = module.environment.location
+  resource_group_name = azurerm_resource_group.workspace-rg.name
 }
 
 resource "azurerm_subnet" "subs" {
   count                     = 2
   name                      = "sub-${count.index}"
-  resource_group_name       = "${azurerm_resource_group.workspace-rg.name}"
-  virtual_network_name      = "${azurerm_virtual_network.vnet.name}"
-  address_prefix            = "${cidrsubnet(element(azurerm_virtual_network.vnet.address_space,0), 1, count.index)}"
-  network_security_group_id = "${azurerm_network_security_group.nsg.id}"
+  resource_group_name       = azurerm_resource_group.workspace-rg.name
+  virtual_network_name      = azurerm_virtual_network.vnet.name
+  address_prefix            = cidrsubnet(element(azurerm_virtual_network.vnet.address_space,0), 1, count.index)
+  network_security_group_id = azurerm_network_security_group.nsg.id
   service_endpoints = [
     "Microsoft.Sql",
     "Microsoft.Storage",
@@ -61,6 +61,6 @@ resource "azurerm_subnet" "subs" {
 
 resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
   count = 2
-  network_security_group_id = "${azurerm_network_security_group.nsg.id}"
-  subnet_id = "${element(azurerm_subnet.subs.*.id, count.index)}"
+  network_security_group_id = azurerm_network_security_group.nsg.id
+  subnet_id                 = element(azurerm_subnet.subs.*.id, count.index)
 }
